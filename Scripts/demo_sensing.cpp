f3004527846f8a16ac81f18c2b8f5acb3ad37ef2
TriNguyenDuc <163371562+CodelikeC@users.noreply.github.com>
2025-05-01 10:15:15 +0700

    Update demo_sensing.cpp

--- Scripts/demo_sensing.cpp
+++ Scripts/demo_sensing.cpp
@@ -1,2 +1,72 @@
 #include <iostream> 
 using namespace std; 
+
+#include <vector> 
+#include <string> 
+
+#include <unordered_map>
+
+#include "../modules/preprocessing/data_Cleaner.h"
+#include "../modules/preprocessing/feature_extraction.h"
+
+#include "../utils/logger.h"
+
+using namespace adaptive_ai;  
+using namespace preprocessing;
+
+using namespace feature;
+
+int main ()
+{
+    Logger :: info("=== Demo : Human-AI sensing and processing ===");
+
+    // tao du lieu mo phong tu nguoi dung .. // 
+    vector<std::unordered_map<std::string, std::string>> user_sessions = {
+        {
+            {"user_id", "alice_01"},
+            {"emotion", "happy"},
+            {"activity", "study"},
+            {"time_spent", "95"},
+            {"location", "home"},
+        },
+        {
+            {"user_id", "bob_22"},
+            {"emotion", "tired"},
+            {"activity", "gaming"},
+            {"time_spent", "140"},
+            {"location", "cafe"},
+        },
+        {
+            {"user_id", "charlie_99"},
+            {"emotion", "neutral"},
+            {"activity", "reading"},
+            {"time_spent", "80"},
+            {"location", "library"},
+        }
+    };
+     // clean + feature extraction cho tung phien .. 
+    DataCleaner cleaner;
+    FeatureExtractor extractor; 
+
+    int index = 1 ; 
+    for (const auto &raw_data : user_sessions)
+    {
+        Logger :: info("Session", index, ":Raw Input");
+        for (const auto &[k , v]: raw_data)
+        {
+            cout <<"-" << k << "=" << v << endl; 
+        }
+
+        auto cleaned = cleaner.clean(raw_data); 
+        auto features = extractor.extract_features(cleaned); 
+        // double features; 
+        cout << "=> Feature vector:" << endl;
+        for (double f : features)
+        cout <<f << ""; 
+        cout << endl;
+        ++ index; 
+    }
+
+    Logger :: info ("== Demo sensing Completes");
+    return 0; 
+}
