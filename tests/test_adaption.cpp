42efeac335284d8e99ade79af2e5e503fd06a9a2
TriNguyenDuc <163371562+CodelikeC@users.noreply.github.com>
2025-05-03 21:21:27 +0700

    Update test_adaption.cpp

--- tests/test_adaption.cpp
+++ tests/test_adaption.cpp
@@ -1,2 +1,34 @@
 #include <iostream> 
 using namespace std; 
+
+#include "../core/adaption.h"
+#include "../core/adaption.cpp"
+
+int main ()
+{
+    cout <<"=============================" << endl;
+    cout <<"test adaption for Adaptive AI" << endl;
+    cout <<"=============================" << endl;
+
+    // Gọi object cho file adaption .. // 
+    AdaptiveEngine engine;
+    
+    // Mo phong khoi dong he thong .. // 
+    engine.initialize();  
+
+    // Mo phong trang thai thay doi .. // 
+    string env1 = "High CPU detected"; 
+    engine.adapt(env1); 
+
+    string env2 = "Unsual network from port 4444";
+    engine.adapt(env2); 
+    
+    string env3 = "Systems resource levels Back to Normal"; 
+    engine.adapt(env3); 
+
+    // tat he thong .. // 
+    engine.ShutDown(); 
+
+    cout  <<"Test adaptive completed" << endl; 
+    return 0; 
+}
