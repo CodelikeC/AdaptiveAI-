457d5a1e9506671e564c79849538634c5d044188
TriNguyenDuc <163371562+CodelikeC@users.noreply.github.com>
2025-06-01 10:01:32 +0700

    Update node.h

--- Server/node.h
+++ Server/node.h
@@ -1,2 +1,50 @@
+#ifndef NODE_H
+#define NODE_H 
+
 #include <iostream> 
 using namespace std; 
+
+#include <string>
+#include <vector>
+
+class Node 
+{
+    public: 
+    Node(const string &id, const string &ip, int port);
+    ~Node(){}; 
+    
+    string getID() const 
+    {
+        return id_;
+    }
+    string getIP() const 
+    {
+        return ip_; 
+    }
+
+    int getPort() const 
+    {
+        return port_; 
+    }
+
+    bool isActive() const 
+    {
+        return active_; 
+    }
+
+    void setActive(bool status)
+    {
+        active_ = status; 
+    }
+
+    private: 
+    string ip_;
+    string id_; 
+    bool active_; 
+    int port_; 
+
+};
+
+
+#endif 
+
