e107f9ddf3e2c6df9b8045b5ca2fadb6490dca25
TriNguyenDuc <163371562+CodelikeC@users.noreply.github.com>
2025-06-01 12:00:37 +0700

    Update server_core.h

--- Server/server_core.h
+++ Server/server_core.h
@@ -11,18 +11,32 @@ using namespace std;
 #include "../core/adaption.h"
 #include "../modules/sensor/network_sensor.h"
 #include "../modules/communication/repc_client.h"
-#include "node_manager.h"
 
+#include "node_manager.h"
+#include "node.h"
+#include "sync.h"
+#include "p2p_communication.h"
+#include "adaptive_ai.h"
 
 class ServerCore 
 {
 private:           
+Node localNode_; 
+NodeManager NodeManager_;
+Sync sync_; 
+P2PCommunication p2p_; 
+AdaptiveAI adaptiveAi_; 
+bool hitlApprovedSelfDestruct_;
 
 public:
-    ServerCore();
-    void startServer();
-    void processData(const std::vector<string>& data);
-    void syncNodes();
+    ServerCore(const string &nodeId, const string &ip, int port);
+    ~ServerCore() ; 
+    void start();
+    void processData(const string& data, AdaptiveAI :: State state);
+    void stop();
+    void handleHITLResponse(const string &response); 
+    void cooldownDatabase(); 
+    void expandServer(const string &externalSystem);
 };
 
 #endif // SERVER_CORE_H
