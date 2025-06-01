26885093800a4587192bbf4cee33f1b8fd7041b3
TriNguyenDuc <163371562+CodelikeC@users.noreply.github.com>
2025-06-01 10:02:56 +0700

    Update sync.h

--- Server/sync.h
+++ Server/sync.h
@@ -1,3 +1,32 @@
+#ifndef SYNC_H
+#define SYNC_H 
+
 #include <iostream> 
 using namespace std; 
 #include <string> 
+
+#include <thread> 
+#include <atomic>
+
+#include "node_manager.h"
+
+class Sync
+{
+    public: 
+    Sync(NodeManager &manager); 
+    ~Sync(); 
+
+    void startSync(); 
+    void stopSync(); 
+    void syncData(const string &sourceId, const string &data );
+
+    private:
+    NodeManager &nodeManager_; 
+    atomic<bool> running_; 
+    thread syncThread; 
+
+    void syncLoop(); 
+}; 
+
+
+#endif 
