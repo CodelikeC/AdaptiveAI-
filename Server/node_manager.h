847af683643f8926df0ac589cb8e8935c185f785
TriNguyenDuc <163371562+CodelikeC@users.noreply.github.com>
2025-06-01 10:02:00 +0700

    Update node_manager.h

--- Server/node_manager.h
+++ Server/node_manager.h
@@ -1,2 +1,28 @@
+#ifndef NODE_MANAGER_H 
+#define NODE_MANAGER_H 
 #include <iostream> 
 using namespace std; 
+
+#include <vector> 
+#include <mutex> 
+#include "node.h"
+
+class NodeManager 
+{
+    public:  
+    NodeManager();
+    ~NodeManager(); 
+
+    void addNode(const Node &node); 
+    void removeNode(const string &nodeID); 
+    Node *getNode(const string &nodeID);
+    vector<Node> getActiveNodes() const;  
+    void updateNodeStatus(const string &nodeID, bool status); 
+
+    private: 
+    vector<Node> nodes_; 
+    mutable mutex mutex_; 
+};
+
+#endif 
+
