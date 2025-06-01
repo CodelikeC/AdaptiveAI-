337778a4716927b6894eed2ddfeb80949f365ce8
TriNguyenDuc <163371562+CodelikeC@users.noreply.github.com>
2025-06-01 10:02:26 +0700

    Update p2p_communication.h

--- Server/p2p_communication.h
+++ Server/p2p_communication.h
@@ -1,4 +1,27 @@
+#ifndef P2P_COMMUNICATION_H 
+#define P2P_COMMUNICATION_H 
+
+#include "node.h"
 #include <iostream> 
 using namespace std ;
 
 #include <string> 
+
+
+class P2PCommunication
+{
+    public : 
+    P2PCommunication(const Node &localNode);
+    ~P2PCommunication(); 
+
+    bool sendMessage(const string &targetIP, int targetPort, const string &message);
+
+    string recieveMessage(); 
+
+    private: 
+    Node localNode_ ; 
+}; 
+
+
+
+#endif 
