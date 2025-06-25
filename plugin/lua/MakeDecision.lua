8c55c8377045b2b294a236066578bf86e56ef03f
TriNguyenDuc <163371562+CodelikeC@users.noreply.github.com>
2025-06-25 10:32:32 +0700

    Update MakeDecision.lua

---
+++ plugin/lua/MakeDecision.lua
@@ -0,0 +1,14 @@
+print("Make Decision")
+
+function Get_Context()
+    -- trich xuat thong tin he thong hien tai ..// 
+end 
+
+function Apply_logic(logic)
+    -- ap dung ly thuyet moi neu duoc duyet .. 
+end
+
+function Log_decision()
+    -- GHi log/ hanh vi duoc chon va li do 
+end
+
