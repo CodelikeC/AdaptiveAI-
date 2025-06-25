0c6d93000b91f46712fb87b82656e83b76e5a62b
TriNguyenDuc <163371562+CodelikeC@users.noreply.github.com>
2025-06-25 10:32:50 +0700

    Update Rollback.lua

---
+++ plugin/lua/Rollback.lua
@@ -0,0 +1,13 @@
+print("Rollback")
+
+function Get_last_safe_logic()
+    -- Truy hoi phien ban logic an toan cuoi cung.. //
+end 
+
+function Revert_to_lastLogic()
+    -- quay ve logic truoc neu Logic hien tai tiem an rui ro
+end
+
+function Notify_again()
+    -- Gui canh bao  cho nguoi dung/admin hoac ghi log..
+end
