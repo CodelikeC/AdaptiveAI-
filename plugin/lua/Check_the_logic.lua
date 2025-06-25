f10c6e0db353377a201d449ce8a1b765463655eb
TriNguyenDuc <163371562+CodelikeC@users.noreply.github.com>
2025-06-25 14:21:35 +0700

    Update Check_the_logic.lua

--- plugin/lua/Check_the_logic.lua
+++ plugin/lua/Check_the_logic.lua
@@ -1,11 +1,27 @@
-print("Check the logic of Lua")
+print("Check the logic [].....")
 
 function Check_integrity(logic)
-    end 
+    if logic and logic.signature == "valid" then 
+        return true 
+    else 
+        return false
+    end
+end
 
 function Simulate_behavior(logic)
+    print(
+        "Simulating logic:", logic.name or "unnamed"
+    )
+    if logic.risk_score and logic.risk_score < 0.5 then 
+        return true 
+    else 
+        return false 
     end 
+end 
 
-function Is_logic_safe()
-    
-    end 
+function Is_logic_safe(logic)
+    local ok_intergrity = Check_integrity(logic)
+    local ok_behavior = Simulate_behavior(logic)
+    return ok_behavior and ok_intergrity
+end 
+
