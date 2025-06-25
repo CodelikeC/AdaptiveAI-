c93ac5198d325fe15b25590e26008e64680add78
TriNguyenDuc <163371562+CodelikeC@users.noreply.github.com>
2025-06-25 10:34:03 +0700

    Update killswitchnewLogic.lua

---
+++ plugin/lua/killswitchnewLogic.lua
@@ -0,0 +1,13 @@
+print("kill new Logic")
+
+function Activate_killswitch()
+    -- Dung moi logic moi dang thuc hien 
+end 
+
+function Isolate_process()
+    -- Co lap logic dong ngo khoi he thong 
+end 
+
+function Emergency_shutdown(reason)
+    -- Tot hoac khoa he thong trong tinh huong khan cap
+end 
