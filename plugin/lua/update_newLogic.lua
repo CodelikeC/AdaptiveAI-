24b9a12605e3955829a1e15c4ae1b644b50d938d
TriNguyenDuc <163371562+CodelikeC@users.noreply.github.com>
2025-06-25 14:26:44 +0700

    Update update_newLogic.lua

--- plugin/lua/update_newLogic.lua
+++ plugin/lua/update_newLogic.lua
@@ -1,15 +1,37 @@
 print("update_new_lua")
 
-function Load_new_logic(path)
+unction Load_new_logic(path)
+print("Loading new logic", path)
+-- simualate logic has been loaded 
+local logic 
+=
+{
+    name ="New Logic",
+    version = "1.0", 
+    signature = "valid", 
+    risk_score = 0.0
+}
+return logic
+end 
 
+function Validate_metadata(logic)
+    -- xac minh metadata xem co bi gia mao khong? 
+    if logic and logic.version then 
+        return true 
+    else 
+        return false
     end 
-
-function Validate_metadata()
-
-    end 
+end 
 
 function StoreLogic(logic)
+    print("Name:", logic.name)
+    print("Version:", logic.version)
 
-    end 
+    print("risk_score", logic.risk_score)
+    print ("signature :", logic.signature)
+    
+    return true 
+end 
+
 
 
