bac73ba49f0c67d58b029fd54fe00e47c0dbb84f
TriNguyenDuc <163371562+CodelikeC@users.noreply.github.com>
2025-08-29 15:16:02 +0700

    Update self_defense.h

--- modules/protection/self_defense.h
+++ modules/protection/self_defense.h
@@ -4,20 +4,27 @@ using namespace std;
 
 #include <algorithm> 
 #include <vector> 
+#include <functional>
 
-namespace adaptive_ai
+namespace adaptive_ai 
 {
     class SelfDefense
     {
         public:
-        SelfDefense(); 
-        ~SelfDefense();
+        // optional logger callback : logger (level, msg)
+        using Logger = std:: function<void(const std:: string &lable, const std:: string &msg)>;
+        explicit SelfDefense (Logger logger = nullptr); 
 
-        // kich hoat co che tu phan ung .. // 
-        void counter_intrusion(const vector<string>&threats);
-        
-        protected: 
-        void isolate_thread(const std::string& id);
-        void log_defense_action(const std::string& action);
-    }; 
-}
+        ~SelfDefense();  
+
+        // kich hoat co che doi pho, tra ve list hanh dong thuc hien ,,,
+        std :: vector< std :: string> conunter_instrusion(const std :: vector<std:: string> &threats); 
+
+        private: 
+        void isolate_thread(const std :: string &id); 
+        void log_defense_action(const std:: string &action); 
+
+        Logger logger_; 
+
+    };
+} // namespace adaptive_ai .. // 
