4677c70a252e44cd2f06ec79cbcba6353436ceb2
TriNguyenDuc <163371562+CodelikeC@users.noreply.github.com>
2025-10-15 16:53:30 +0700

    Add files via upload

---
+++ tests/Intergration_test_server.cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+
+#include "../server/node.h"
+#include "../server/node_manager.h"
+
+#include "../server/p2p_communication.h"
+#include "../server/server_core.h"
+
+#include "../server/sync.h"
+#include "../server/mem_check.h"
+
+using namespace std; 
+
+int main ()
+{
+    cout <<"[TEST] Starting intergration test...\n"; 
+
+    // CORE AI unit .. 
+    
+    
+    // Node + manager 
+    Node node1(); 
+
+    // P2p Communication..// 
+    
+}
