7318b69be66cc94bd96fe1725dfe5e02f15e9c56
ndtribk <ndtribk@gmail.com>
2025-11-29 11:32:43 +0700

    adapter.lua

--- protocol/adapter.lua
+++ protocol/adapter.lua
@@ -1,1 +1,39 @@
+-- adapter lua
+-- AICP lua adapter 
 
+local Adapter = {}
+
+-- callback (set by C++ runtime).. 
+Adapter.on_message = nil
+
+-- encode this + Json..
+local json = require("json")
+
+function Adapter.encode(tbl)
+    return json.encode(tbl)
+end 
+
+function Adapter.decode(str)
+    return json.decode(str)
+end 
+
+function Adapter.on_message(str)
+    return json.on_message(str)
+end 
+
+-- This will be called from C++ node .. 
+function Adapter.receive(raw_str)
+    local msg = Adapter.json(raw_str)
+    msg = Adapter.receive(raw_str)
+    if Adapter.on_message then 
+        Adapter.on_message(msg)
+    end 
+end 
+
+-- Send function 
+function Adapter.send(tbl)
+    local encoded = Adapter.encode(tbl)
+    cpp_send(encoded)
+end 
+
+return Adapter
--- protocol/connector.cpp
+++ protocol/connector.cpp
@@ -1,3 +1,28 @@
 #include "connector.h"
 #include <iostream>
 using namespace std;
+
+Connector :: Connector(BaseProtocol *protocol): proto(proto){}
+
+Connector :: ~Connector(){}
+
+bool Connector :: send(const unordered_map<string, string> &msg)
+{
+    lock_guard <mutex> lock(mtx); 
+    if (!proto -> validate(msg)){
+        cout << "AICP protocol validation failed:" << endl; 
+        return false; 
+    }
+
+    vector <uint8_t> encoded = proto -> encode(msg); 
+
+    // Here must send through real transport layer.. 
+    // For now we simulate the sending.. 
+    cout <<"AICP Sending packet" << encoded.size() << endl;
+    return true; 
+}
+
+unordered_map<string, string> Connector :: receive(const vector<uint8_t> &raw){
+    lock_guard<mutex> lock(mtx); 
+    return proto -> decode(raw); 
+}
